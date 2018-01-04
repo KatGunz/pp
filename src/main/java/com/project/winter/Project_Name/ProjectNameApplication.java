1e5e2a5439b17a3ab1fabee13d190a90f0de3b08
LesLiu94 <ll545@scarletmail.rutgers.edu>
2018-01-04 00:19:14 -0500

    Reduction of unnecessary wrapper

---
+++ src/main/java/com/project/winter/Project_Name/ProjectNameApplication.java
@@ -0,0 +1,12 @@
+package com.project.winter.Project_Name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNameApplication.class, args);
+	}
+}
---
+++ src/test/java/com/project/winter/Project_Name/ProjectNameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.winter.Project_Name;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectNameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
