849cce9e94a02f9ff139711d45ce6ee23215a7a3
Michael_Bauer <michaelxbauer@gmail.com>
2018-01-14 17:43:06 -0500

    added application properties, and started making beans for the java code

---
+++ src/main/java/Beans/Brand.java
@@ -0,0 +1,19 @@
+package Beans;
+
+public class Brand {
+    private long brandID;
+    private String brandName;
+    
+    public long getBrandID() {
+        return brandID;
+    }
+    public String getBrandName() {
+        return brandName;
+    }
+    public void setBrandID(long brandID) {
+        this.brandID = brandID;
+    }
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+}
