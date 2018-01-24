e23d00e1122fc1dbe46dfb730231507a5d0d5219
MSBauer <michaelxbauer@gmail.com>
2018-01-23 21:45:44 -0500

    added dao files in to the dao package; began writing daos

---
+++ src/main/java/DAO/FoodDAO.java
@@ -0,0 +1,13 @@
+package DAO;
+
+import Beans.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FoodDAO extends JpaRepository<Food,Long>{
+    List<Food> findById(Long Id);
+    List<Food> findByFood(Food food);
+}
