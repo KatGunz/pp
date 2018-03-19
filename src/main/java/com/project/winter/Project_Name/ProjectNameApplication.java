76cc2ae861b84b564b647360477cdacf1ebfc87c
MSBauer <michaelxbauer@gmail.com>
2018-03-18 20:34:18 -0400

    converted to InnoDB clusters and adjusted some increment properties to stay congruent with current data scripts; cluster nodes are directly accessible locally and remotely; MySQL router is only accessible locally, need to find out how to fix and incorporate load balancing for the db custer

--- src/main/java/com/project/winter/Project_Name/ProjectNameApplication.java
+++ src/main/java/com/project/winter/Project_Name/ProjectNameApplication.java
@@ -8,5 +8,6 @@ public class ProjectNameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectNameApplication.class, args);
+		//any main method setup for the application
 	}
 }
