5a7ff3d26dff968964d34620ad2e9d73585ffea4
LesLiu94 <ll545@scarletmail.rutgers.edu>
2018-05-21 23:43:06 -0400

    refactored names for some of the sql scripts

--- sql scripts/MakePP.sql
+++ sql scripts/MakePP.sql
@@ -3,58 +3,58 @@ CREATE SCHEMA `Personal_Project_Food`;
 USE `Personal_Project_Food`;
 
 
-CREATE TABLE Brand
+CREATE TABLE brand
 (
-    BrandId INT NOT NULL AUTO_INCREMENT,
-    BrandName VARCHAR(120) NOT NULL,
-    CONSTRAINT PK_Brand Primary Key (BrandId),
-    CONSTRAINT uniqueBrandName UNIQUE (BrandName)
+    Brand_Id INT NOT NULL AUTO_INCREMENT,
+    Brand_Name VARCHAR(120) NOT NULL,
+    CONSTRAINT PK_Brand Primary Key (Brand_Id),
+    CONSTRAINT uniqueBrandName UNIQUE (Brand_Name)
 );
 
 /* Nutrional values are in units of mg */
 
-CREATE TABLE FOOD
+CREATE TABLE food
 (
-    FoodID INT NOT NULL AUTO_INCREMENT,
-    FoodName VARCHAR(120) NOT NULL,
+    Food_Id INT NOT NULL AUTO_INCREMENT,
+    Food_Name VARCHAR(120) NOT NULL,
     Calories INT,
-    TotalFat INT,
+    Total_Fat INT,
     Cholesterol INT,
     Sodium INT,
-    TotalCarbs INT,
+    Total_Carbs INT,
     Protein INT,
-    VitaminA INT,
-    VitaminB INT,
-    VitaminC INT,
-    VitaminD INT,
+    Vitamin_A INT,
+    Vitamin_B INT,
+    Vitamin_C INT,
+    Vitamin_D INT,
     Calcium INT,
     Iron INT,
-    CONSTRAINT PK_Food Primary Key (FoodID),
-    CONSTRAINT uniqueFoodName UNIQUE (FoodName)
+    CONSTRAINT PK_Food Primary Key (Food_Id),
+    CONSTRAINT uniqueFoodName UNIQUE (Food_Name)
 );
 
-CREATE TABLE UnhealthyToHealthy
+CREATE TABLE unhealthytohealthy
 (
-    UnhealthyFoodID INT NOT NULL,
-    HealthyFoodID INT NOT NULL,
+    Unhealthy_Food_Id INT NOT NULL,
+    Healthy_Food_Id INT NOT NULL,
     Constraint PK_UnhealthyToHealthy Primary Key
     (
-        UnhealthyFoodID,
-        HealthyFoodID
+        Unhealthy_Food_Id,
+        Healthy_Food_Id
     ),
-    FOREIGN KEY (UnhealthyFoodID) REFERENCES Food(FoodID) ON DELETE CASCADE,
-    FOREIGN KEY (HealthyFoodID) REFERENCES Food(FoodID) ON DELETE CASCADE
+    FOREIGN KEY (Unhealthy_Food_Id) REFERENCES food(Food_Id) ON DELETE CASCADE,
+    FOREIGN KEY (Healthy_Food_Id) REFERENCES food(Food_Id) ON DELETE CASCADE
 );
-CREATE TABLE FoodToBrand
+CREATE TABLE foodtobrand
 (
-    FoodID INT NOT NULL,
-    BrandID INT NOT NULL,
+    Food_Id INT NOT NULL,
+    Brand_Id INT NOT NULL,
     Constraint PK_FoodToBrand Primary Key
     (
-        FoodID,
-        BrandID
+        Food_Id,
+        Brand_Id
     ),
-    FOREIGN KEY (FoodID) REFERENCES Food(FoodID) ON DELETE CASCADE,
-    FOREIGN KEY (BrandID) REFERENCES Brand(BrandID) ON DELETE CASCADE
+    FOREIGN KEY (Food_Id) REFERENCES food(Food_Id) ON DELETE CASCADE,
+    FOREIGN KEY (Brand_Id) REFERENCES brand(Brand_Id) ON DELETE CASCADE
 );
 
