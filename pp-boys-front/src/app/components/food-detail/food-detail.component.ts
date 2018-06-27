ccc61fc3bff359f1980f541916eb59ae02e9eb54
KatGunz <michaelxbauer@gmail.com>
2018-06-27 01:07:35 -0400

    removed unused import

--- pp-boys-front/src/app/components/food-detail/food-detail.component.ts
+++ pp-boys-front/src/app/components/food-detail/food-detail.component.ts
@@ -3,7 +3,6 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FoodDTO } from '../../domain/Food';
 import { FoodService } from '../../services/services.food/food.service';
-import { Observable } from 'rxjs';
 import { MessageService } from '../../services/services.message/message.service'
 
 @Component({
