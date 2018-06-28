98c7c0d4e4c05129c96ef364a220904c2f428224
LesLiu94 <ll545@scarletmail.rutgers.edu>
2018-06-28 18:42:29 -0400

    attempted to implement functionality to the searchbar of navbar and changed dashboard's search button as well as removed box-shadow

--- pp-boys-front/src/app/app.component.ts
+++ pp-boys-front/src/app/app.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { FoodService } from 'src/app/services/services.food/food.service';
+import { FoodDTO } from '../app/domain/Food';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DataService } from '../app/services/services.data/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [
-    './app.component.css'
-  ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Food Suggestion App';
+  searchQuery: string;
+  healthyFoods: FoodDTO[];
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private foodService: FoodService,
+    private snackBar: MatSnackBar,
+    private dataService: DataService
+  ){}
+
+  ngOnInit(){
+  }
+  private onSubmit(searchQuery: string): void {
+    if(!searchQuery){
+      return;
+    }
+    this.searchQuery = searchQuery;
+    this.suggestFoods(this.searchQuery);
+  }
+  private suggestFoods(unhealthyFood: string): void {
+    this.foodService.suggestFoods(unhealthyFood)
+      .subscribe(healthyFoods => this.passResults(healthyFoods));
+  }
+  private passResults(healthyFoods: FoodDTO[]): void {
+    if(healthyFoods){
+      this.healthyFoods = healthyFoods;
+      var isFull = this.dataService.assignToNavFoodResultCubby(healthyFoods);
+      if(isFull){
+        console.log("Cubby was full!");
+        return;
+      }
+      this.router.navigate([`suggested-foods/${this.searchQuery}`]);
+    }
+    else{
+      this.snackBar.open("No Results Found.", null, {
+        duration: 3000
+      });
+    }
+  }
+  
+  
 }
