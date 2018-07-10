8b6fc686fce2a2f523300e1bccfb8665b3f2624a
KatGunz <michaelxbauer@gmail.com>
2018-07-10 00:32:22 -0400

    mid-way addressing the navigation issues

--- pp-boys-front/src/app/components/suggested-foods/suggested-foods.component.ts
+++ pp-boys-front/src/app/components/suggested-foods/suggested-foods.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { FoodDTO } from '../../domain/Food';
 import { Location } from '@angular/common';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd, Event, NavigationStart, NavigationError, NavigationCancel } from '@angular/router';
 import { FoodService } from '../../services/services.food/food.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subscription } from 'rxjs';
@@ -28,7 +28,7 @@ export class SuggestedFoodsComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.setFoodSuggestions();
-    this.subscription = this.router.events.subscribe((val)=>this.setFoodSuggestions());
+    this.subscription = this.router.events.subscribe((event)=>this.setFoodSuggestions);
   }
   ngOnDestroy(){
     this.subscription.unsubscribe();
@@ -36,17 +36,31 @@ export class SuggestedFoodsComponent implements OnInit, OnDestroy {
   goBack():void{
     this.location.back();
   }
-  setFoodSuggestions(){
-    console.log(this.route.snapshot.paramMap.get('searchQuery'));
-    this.foodService.suggestFoods(this.route.snapshot.paramMap.get('searchQuery'))
-      .subscribe(healthyFoods => this.initFoodSuggestions(healthyFoods));
+  handleRouteChange(event: Event):void{
+    this.setFoodSuggestions
+    // if(event instanceof NavigationStart){
+    //   this.setFoodSuggestions
+    // }else if (event instanceof NavigationEnd){
+    //   this.setFoodSuggestions
+    // } else if(event instanceof NavigationError){
+    //   this.setFoodSuggestions
+    // } else if(event instanceof NavigationCancel){
+    //   this.setFoodSuggestions
+    // }
   }
-  initFoodSuggestions(healthyFoods: FoodDTO[]):void{
+  setFoodSuggestions(){
+    var searchQuery = this.route.snapshot.paramMap.get('searchQuery');
+    console.log(searchQuery);
+    this.foodService.suggestFoods(searchQuery)
+      .subscribe(healthyFoods => this.initFoodSuggestions(healthyFoods, searchQuery));
+  }
+  initFoodSuggestions(healthyFoods: FoodDTO[], searchQuery):void{
     if(!healthyFoods){
       this.matSnackBar.open("No Results Found","",{duration:3000});
       healthyFoods = [];
     }
     this.initDataSource(healthyFoods);
+    this.router.navigate([`suggested-foods/${searchQuery}`]);
   }
   initDataSource(healthyFoods: FoodDTO[]): void{
     this.dataSource = new MatTableDataSource(healthyFoods);
